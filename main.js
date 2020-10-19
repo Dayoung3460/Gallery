6f83c548c972e6bfb3c7c2dc0c277c60fff9fb91
Dayoung <ekdud3460@naver.com>
2020-10-19 19:42:55 +0900

    add background color changes

--- main.js
+++ main.js
@@ -2,9 +2,23 @@
 
 const hightlight = document.querySelector(".highlight");
 const previews = document.querySelectorAll(".preview img");
+const colors = ["antiquewhite", "blueviolet", "paleVioletRed", "INDIANRED", "TOMATO", "DARKKHAKI", "MEDIUMPURPLE"];
+const btn = document.querySelector('.btn')
+const colorName = document.querySelector('.colorName');
+const descriptionContainer = document.querySelector('.description');
 
-
+btn.addEventListener('click', changeBackColor);
 imageGallery();
+
+function changeBackColor(){
+    const randomNumber = getRandomNumber();
+    descriptionContainer.style.backgroundColor = colors[randomNumber];
+    colorName.textContent = colors[randomNumber];
+}
+
+function getRandomNumber(){
+    return Math.floor(Math.random()*colors.length);
+}
 
 function imageGallery(){
     previews.forEach(preview => {
